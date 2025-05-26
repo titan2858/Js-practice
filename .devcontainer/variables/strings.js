f60a4756865d93d2214629aa5ceae922023182e8
Hrishikesh <rushibankapur22@gmail.com>
2025-05-26 05:58:22 +0000

    string methods

--- .devcontainer/variables/strings.js
+++ .devcontainer/variables/strings.js
@@ -11,9 +11,27 @@ console.log(`hello my name is ${name} and my age is ${age}`);
 let a=new String("Hello World");
 console.log(a.charCodeAt(3));
 console.log(a.concat(3));
-console.log(a.endsWith('rld',10));
+console.log(a.endsWith('rld'));
 console.log(a.startsWith('Hello'));
-console.log(a);
+console.log(a.slice(-5,-1));
+console.log(a.slice(3,7));
+console.log(a.includes("kots"));
+console.log(a.substring(3,8));
+console.log(a.substr(4,3));
+console.log(a.toUpperCase());
+
+let b=new String("      the   re");
+console.log(b.trim());
+
+
+console.log(a);  //original string nerver changes since string is primitive data type.
+
+
+
+
+
+
+
 
 
 
